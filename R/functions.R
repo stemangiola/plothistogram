5b4f0063e4f8fdc9a177e2d880b5dfe636d2e3a2
stemangiola <mangiolastefano@gmail.com>
2020-04-23 10:37:49 +1000

    first commit

---
+++ R/functions.R
@@ -0,0 +1,36 @@
+#' Creates an histogram from a data frame
+#'
+#' \lifecycle{experimental}
+#'
+#' @description create_histogram() creates a `ggplot` object from a data frame
+#'
+#' @import ggplot2
+#' @importFrom magrittr "%>%"
+#' @importFrom tibble tibble
+#'
+#' @name create_histogram
+#'
+#' @param .data A `tbl_df` including ...
+#' @param .abundance A column name for the variable to be plotted
+#'
+#' @details This function creates a ggplot histogram from a data frame
+#'
+#' @return A `ggplot` object
+#'
+#' @examples
+#'
+#' create_histogram(
+#'    tibble::tibble(counts = 1:100),
+#'    counts
+#' )
+#'
+#' @export
+create_histogram = function(.data, .abundance) {
+
+  .abundance = enquo(.abundance)
+
+  .data %>%
+    ggplot(aes(!!.abundance)) +
+    geom_histogram()
+
+}
